0f78c3f196bced18ec41493646de1cb8f89ad09b
AlejandroMoP <alejandro.morales@ing.uchile.cl>
2021-05-07 18:42:46 -0400

    fixing triplet loss

--- tarea2/losses.py
+++ tarea2/losses.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn as nn
 def triplet_loss(margin = 20):
-    def loss(y_true, y_pred):
-        #y_true will be used for training cross_entropy
-        e_a, e_p, e_n = torch.split(y_pred, split_size_or_sections = 3, dim = 1)
+    def loss(e_a, e_p, e_n):
         d_p = torch.sqrt(torch.sum(torch.square(e_a - e_p), 2))
         d_p_hard = torch.mean(d_p)
         d_n = torch.sqrt(torch.sum(torch.square(e_a - e_n), 2))
@@ -18,15 +16,16 @@ def crossentropy_loss(y_true, y_pred):
     This is the classical categorical crossentropy
     """
     ce = nn.CrossEntropyLoss()
-    out = ce(y_true, y_pred, from_logits=True)
+    out = ce(y_true, y_pred)
     return out
 
-def crossentropy_triplet_loss(y_true, y_pred):
-    y_true_a, y_true_p, y_true_n = torch.split(y_true, split_size_or_sections = 3, dim = 1)
-    cl_a, cl_p, cl_n = torch.split(y_pred, split_size_or_sections = 3, dim = 1)
+def crossentropy_triplet_loss(y_true_a, y_true_p, y_true_n, y_pred_a, y_pred_p, y_pred_n):
+
     ce = nn.CrossEntropyLoss()
-    ce_a = ce(torch.squeeze(y_true_a), torch.squeeze(cl_a), from_logits=True)
-    ce_p = ce(torch.squeeze(y_true_p), torch.squeeze(cl_p), from_logits=True)
-    ce_n = ce(torch.squeeze(y_true_n), torch.squeeze(cl_n), from_logits=True)
+    ce_a = ce(torch.squeeze(y_true_a), torch.squeeze(y_pred_a))
+    ce_p = ce(torch.squeeze(y_true_p), torch.squeeze(y_pred_p))
+    ce_n = ce(torch.squeeze(y_true_n), torch.squeeze(y_pred_n))
     loss = (ce_a + ce_p + ce_n) / 3.0
     return loss
+
+#def contrastive_loss():
