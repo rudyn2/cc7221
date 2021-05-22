ffa64203cd34b97bdbaab59b5c1efd36e50f7155
AlejandroMoP <alejandro.morales@ing.uchile.cl>
2021-05-22 06:42:26 -0400

    Map, Recall-ratio, Recall-Precision finished and added tests in eval_data

--- tarea2/metrics.py
+++ tarea2/metrics.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+from ranking import Ranker
 
 
 def simple_accuracy(y_true, y_pred):
@@ -40,21 +42,59 @@ def metric_accuracy_siamese(y_true_a, y_true_p, y_true_n, cl_a, cl_p, cl_n):
     return acc
 
 
-def mean_average_precision(y_pred_ranks, y_true):  # o dataset
-    #caso1 ranking contiene la clase de la imagen al igual q true
-    contf = 0
-    for i in range(len(y_true)):
-        cont = 0
-        cont2 = 0
-        ap = 0
-        for j in range(len(y_pred_ranks[i])):
-            if y_true[i] == y_pred_ranks[i][j]:
-                cont += (1 + cont2 )/ (j+1)
-                cont2 += 1
-        contf += cont/len(y_pred_ranks[i])
+def average_precision(ranking: Ranker, path):  # o dataset
+    # caso1 ranking contiene la clase de la imagen al igual q true
+    c, rank = ranking.get_rank(path)
+    cont = 0
+    cont2 = 0
+    for j in range(len(rank)):
+        if c == rank[j][1]:
+            cont += (1 + cont2) / (j + 1)
+            cont2 += 1
 
-    map = contf / len(y_true)
-    return map
+    ap = cont / cont2
+    #print(cont2)
+
+    # map = contf / len(y_true)
+    return ap
+
+
+def map(paths: list, ranking):
+    aps = []
+    for i in range(len(paths)):
+        aps.append(average_precision(ranking, paths[i]))
+    return np.mean(aps)
+
+
+def recall_ratio(ranking, path, len_class):
+    c, rank = ranking.get_rank(path)
+    cont = 0
+    x = []
+    y = []
+    for i in range(len(rank)):  # or 2000
+        x.append(i + 1)
+        if rank[i][1] == c:
+            cont += 1
+        y.append(cont / len_class)
+    return x, y
+
+
+def recall_presicion(ranking, path, len_class):
+    recalls = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+    c, rank = ranking.get_rank(path)
+    precision = np.zeros(11)
+    cont = 0
+    cont2 = 0
+
+    for i in range(len(rank)):
+        if rank[i][1] == c:
+            cont = (1 + cont2) / (i + 1)
+            cont2 += 1
+            idx = int((cont2 / len_class) * 10)
+            if cont > precision[idx]:
+                precision[idx] = cont
+    return precision
+
 
 def aux(y_pred_ranks, y_true):  # o dataset
     # caso1 ranking contiene la clase de la imagen al igual q true
@@ -62,25 +102,24 @@ def aux(y_pred_ranks, y_true):  # o dataset
     cont2 = 0
     for i in range(len(y_pred_ranks)):
         if y_true == y_pred_ranks[i]:
-            contf += (1 +cont2) / (i+1)
-            #print(contf)
+            contf += (1 + cont2) / (i + 1)
+            # print(contf)
             cont2 += 1
-    return contf/len(y_pred_ranks)
+    return contf / len(y_pred_ranks)
 
+    # caso 2 ranking tiene el indice de la imagen
+    # contf = 0
+    # for i in range(len(y_pred_ranks)):
+    #   cont = 0
+    #  ap = 0
+    # for j in range(len(y_pred_ranks[i])):
+    #    if y_true[i] == y_pred_ranks[i][j]:
+    #        cont += 1 / j
+    # contf += cont / len(y_pred_ranks[i])
 
+    # map = contf / len(y_true)
+    # return map
 
-    #caso 2 ranking tiene el indice de la imagen
-    #contf = 0
-    #for i in range(len(y_pred_ranks)):
-     #   cont = 0
-      #  ap = 0
-       # for j in range(len(y_pred_ranks[i])):
-        #    if y_true[i] == y_pred_ranks[i][j]:
-        #        cont += 1 / j
-        #contf += cont / len(y_pred_ranks[i])
-
-    #map = contf / len(y_true)
-    #return map
 
 def recall(ranks, y_true):
     contf = 0
