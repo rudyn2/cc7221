b659e05776ea58892340431c6d3b76ea723b59b9
rudyn2 <c01010010@hotmail.com>
2021-05-22 18:04:40 -0400

    update Dockerfile base image

--- tarea2/run_docker.sh
+++ tarea2/run_docker.sh
@@ -3,3 +3,4 @@
 docker run -d --rm -v ~/cachefs/cbir/data:/home/cc7221/data -v ~/cachefs/cbir/weights/home/cc7221/weights \
        -e WANDB_API_KEY=5990125fc96222f63d970131fd574f587ed56e1e --gpus all -e NVIDIA_VISIBLE_DEVICES=1 \
        -it trainer bash
+
