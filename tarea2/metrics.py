a6ac03812c5295c4ac70e73a442935eb8e7ab55b
AlejandroMoP <alejandro.morales@ing.uchile.cl>
2021-05-23 22:11:56 -0400

    Examples images for each class of eval dataset, contrastive case

--- tarea2/metrics.py
+++ tarea2/metrics.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 from ranking import Ranker
-from datasets import FlickrDataset
+from datasets import FlickrDataset2
 
 
 def simple_accuracy(y_true, y_pred):
@@ -43,21 +43,7 @@ def metric_accuracy_siamese(y_true_a, y_true_p, y_true_n, cl_a, cl_p, cl_n):
     return acc
 
 
-def average_precision2(ranking: Ranker, path):  # o dataset
-    # caso1 ranking contiene la clase de la imagen al igual q true
-    c, rank = ranking.get_rank(path)
-    cont = 0
-    cont2 = 0
-    for j in range(len(rank)):
-        if c == rank[j][1]:
-            cont += (1 + cont2) / (j + 1)
-            cont2 += 1
 
-    ap = cont / cont2
-    #print(cont2)
-
-    # map = contf / len(y_true)
-    return ap
 
 def average_precision(c, rank):  # o dataset
     # caso1 ranking contiene la clase de la imagen al igual q true
@@ -76,11 +62,7 @@ def average_precision(c, rank):  # o dataset
     return ap
 
 
-def map2(paths: list, ranking):
-    aps = []
-    for i in range(len(paths)):
-        aps.append(average_precision(ranking, paths[i]))
-    return np.mean(aps)
+
 
 def map(c:list, rank:list):
     aps = []
@@ -88,17 +70,7 @@ def map(c:list, rank:list):
         aps.append(average_precision(c[i], rank[i]))
     return np.mean(aps)
 
-def recall_ratio2(ranking, path, len_class):
-    c, rank = ranking.get_rank(path)
-    cont = 0
-    x = []
-    y = []
-    for i in range(len(rank)):  # or 2000
-        x.append(i + 1)
-        if rank[i][1] == c:
-            cont += 1
-        y.append(cont / len_class)
-    return x, y
+
 
 def recall_ratio_per_query(c, rank, len_class):
     cont = 0
@@ -112,7 +84,7 @@ def recall_ratio_per_query(c, rank, len_class):
     return x, y
 
 def recall_ratio_tot(c:list, rank:list, path_db: str ):#'B:\Flickr\Flickr15K\images'
-    test_flickr_db = FlickrDataset(path_db)
+    test_flickr_db = FlickrDataset2(path_db)
     img_per_class = test_flickr_db._build_groups()
     len_classes = []
     for i in range(1, 34):
@@ -128,21 +100,7 @@ def recall_ratio_tot(c:list, rank:list, path_db: str ):#'B:\Flickr\Flickr15K\ima
 
 
 
-def recall_presicion2(ranking, path, len_class):
-    recalls = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
-    c, rank = ranking.get_rank(path)
-    precision = np.zeros(11)
-    cont = 0
-    cont2 = 0
 
-    for i in range(len(rank)):
-        if rank[i][1] == c:
-            cont = (1 + cont2) / (i + 1)
-            cont2 += 1
-            idx = int((cont2 / len_class) * 10)
-            if cont > precision[idx]:
-                precision[idx] = cont
-    return precision
 
 def recall_presicion_per_query(c,rank, len_class):
     recalls = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
@@ -162,7 +120,7 @@ def recall_presicion_per_query(c,rank, len_class):
     return precision
 
 def recall_prec_tot(c:list, rank:list, path_db: str ):
-    test_flickr_db = FlickrDataset(path_db)
+    test_flickr_db = FlickrDataset2(path_db)
     img_per_class = test_flickr_db._build_groups()
     len_classes = []
     for i in range(1,34):
@@ -175,54 +133,13 @@ def recall_prec_tot(c:list, rank:list, path_db: str ):
     return np.mean(rps,0)
 
 
-def aux(y_pred_ranks, y_true):  # o dataset
-    # caso1 ranking contiene la clase de la imagen al igual q true
-    contf = 0
-    cont2 = 0
-    for i in range(len(y_pred_ranks)):
-        if y_true == y_pred_ranks[i]:
-            contf += (1 + cont2) / (i + 1)
-            # print(contf)
-            cont2 += 1
-    return contf / len(y_pred_ranks)
-
-    # caso 2 ranking tiene el indice de la imagen
-    # contf = 0
-    # for i in range(len(y_pred_ranks)):
-    #   cont = 0
-    #  ap = 0
-    # for j in range(len(y_pred_ranks[i])):
-    #    if y_true[i] == y_pred_ranks[i][j]:
-    #        cont += 1 / j
-    # contf += cont / len(y_pred_ranks[i])
-
-    # map = contf / len(y_true)
-    # return map
 
 
-def recall(ranks, y_true):
-    contf = 0
-    for i in range(len(y_true)):
-        cont = 0
-        for j in range(len(ranks[i])):
-            if y_true[i] == ranks[i][j]:
-                cont += 1
-        contf += cont / len(ranks[i])
-
-    recall = contf / len(y_true)
-    return recall
 
 
-def Precision(ranks, y_true):
-    contf = 0
-    for i in range(len(y_true)):
-        cont = 0
-        for j in range(len(ranks[i])):
-            if y_true[i] == ranks[i][j]:
-                cont += 1
-        contf += cont / len(ranks[i])
 
-    prec = contf / len(ranks)
-    return prec
 
-# def recall(y_true,y_pred) #dudas hacerlo con keras y entradas
+
+
+
+
