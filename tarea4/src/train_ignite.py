815d3ec93fb1b19af1ba0e8b33df1dd3b9fd5e9f
AlejandroMoP <alejandro.morales@ing.uchile.cl>
2021-06-20 00:38:36 -0400

    classes weights like args v3

--- tarea4/src/train_ignite.py
+++ tarea4/src/train_ignite.py
@@ -57,12 +57,13 @@ def run(args):
     if args.loss == 'focal':
         loss = FocalLoss(apply_nonlin=torch.sigmoid)
     elif args.loss == 'wnll':
-        loss = WeightedPixelWiseNLLoss(weights={
-            0: torch.float32(args.weights_c[0]),
-            1: torch.float32(args.weights_c[1]),
-            2: torch.float32(args.weights_c[2]),
-            3: torch.float32(args.weights_c[3])
-        })
+        loss = WeightedPixelWiseNLLoss(weights=args.weights)
+        #loss = WeightedPixelWiseNLLoss(weights={
+        #    0: args.weights_c[0],
+        #    1: args.weights_c[1],
+        #    2: args.weights_c[2],
+        #    3: args.weights_c[3]
+        #})
     else:
         loss = DiceLoss()
     print(colored("[+] Model, optimizer and loss are ready!", "green"))
@@ -175,7 +176,12 @@ if __name__ == '__main__':
     parser.add_argument('--batch-size', default=2, type=int, help="Batch size")
     parser.add_argument('--epochs', default=10, type=int, help="Number of epochs")
     parser.add_argument('--lr', default=0.001, type=float, help='Initial learning rate')
-    parser.add_argument('--weights_c', default=[0.05, 0.35, 0.3, 0.3], type=list, help='weights for classes')
+    parser.add_argument('--weights', default={
+            0: 0.05,
+            1: 0.35,
+            2: 0.3,
+            3: 0.3
+        }, type=dict, help='weights for classes')
 
     torch.cuda.empty_cache()
     args_ = parser.parse_args()
